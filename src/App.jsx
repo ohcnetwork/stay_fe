803a82c1372bfc7a9b1f24e2bc148c7e4a7bc10e
aashnanaushad <36255953+aashnanaushad@users.noreply.github.com>
2020-04-11 16:57:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-import { Login, Register } from "./components/login/index";
+import { Login, Register } from "./components/auth/index";
 
 class App extends React.Component {
   constructor(props) {
