da610c088439dcbcc8401f9a91198ebb621c4aec
psjishnu <psjishnu13@gmail.com>
2020-04-12 11:46:13 +0530

    updated regiser

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -73,19 +73,7 @@ const RoomsFilter = ({ rooms }) => {
         </div>
         {/* end of guests */}
         {/* room price */}
-        <div className="form-group">
-          <label htmlFor="price">room price Rs {price}</label>
-          <input
-            type="range"
-            name="price"
-            min={minPrice}
-            max={maxPrice}
-            id="price"
-            value={price}
-            onChange={handleChange}
-            className="form-control"
-          />
-        </div>
+
         <div className="form-group">
           <label htmlFor="location">room price Rs {price}</label>
           <input
