3b8c8097d5a7e17140dac657dd540b688f917c44
aashnanaushad <36255953+aashnanaushad@users.noreply.github.com>
2020-04-12 20:21:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
-import Lgin from "./components/Lgin";
+import Lgin from "./components/auth";
 
 import Navbar from "./components/Navbar";
 
