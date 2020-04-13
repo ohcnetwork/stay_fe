be7667a774540324435b300d29d2fac3d996ad6e
aashnanaushad <36255953+aashnanaushad@users.noreply.github.com>
2020-04-13 20:15:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React from "react";
-import "./App.css";
-
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/Error";
-import Lgin from "./components/Lgin";
-
-import Navbar from "./components/Navbar";
-
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms/" component={Rooms} />
-        <Route exact path="/rooms/:slug" component={SingleRoom} />
-        <Route exact path="/auth" component={Lgin} />
-        <Route component={Error} />
-      </Switch>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
