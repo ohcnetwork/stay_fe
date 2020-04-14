ab9cff8156ee460ef927a4539413460ef100d40e
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-04-14 19:08:53 +0530

    Add redux for login and app loading
    - Add reducers and actions for login
    - Add a mock app icon
    - Remove user if unauthenticated access

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { userActions } from '../../redux/actions';
 
 import './Navbar.css';
 
 function Navbar() {
+  const user = useSelector(state => state.user);
+
   const [menuActive, setMenuActive] = useState(false);
-  const navEl = useRef(null);
+
+  const dispatch = useDispatch();
 
   function menuToggle() {
     (menuActive)? menuDisable(): setMenuActive(true);
@@ -15,6 +21,12 @@ function Navbar() {
     setMenuActive(false);
   }
 
+  function logoutUser() {
+    localStorage.removeItem('user');
+    dispatch(userActions.logout());
+  }
+
+
   return (
     <header className="navbar">
       <h1>Stay</h1>
@@ -23,11 +35,23 @@ function Navbar() {
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
-      <nav role="navigation" ref={navEl} className={`${menuActive? "is-active": ""}`} onClick={menuDisable}>
-        <Link to ="/" className="nav-link">Home</Link>
-        <Link to ="/about" className="nav-link">About</Link>
-        <Link to ="/login" className="nav-link nav-btn">Login</Link>
-        <Link to ="/register" className="nav-link nav-btn nav-btn-theme">Register</Link>
+      <nav role="navigation" className={`${menuActive? "is-active": ""}`} onClick={menuDisable}>
+        { 
+          (user) 
+        ?
+          <>
+            <Link to="/user" className="nav-link">Dashboard</Link>
+            <Link to="/rooms" className="nav-link">Rooms</Link>
+            <div role="button" onClick={logoutUser} className="nav-link nav-btn nav-btn-theme">Logout</div>
+          </>
+        :
+          <>
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/login" className="nav-link nav-btn">Login</Link>
+            <Link to="/register" className="nav-link nav-btn nav-btn-theme">Register</Link>
+          </>
+        }
       </nav>
     </header>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-function Login() {
+import { userActions } from '../../redux/actions';
+
+function Login(props) {
+
+  const [submitted, setSubmitted] = useState(false);
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: ''
+  });
+  const { username, password } = inputs;
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputs(inputs => ({ ...inputs, [name]: value }));
+  }
+
+  // check login status
+  useEffect(() => { 
+    if (user) {
+      console.log('login.js: a user had already logged in', user);
+      props.history.push('/user');
+    }
+  }, [user, props.history]);
+
+  function handleSubmit(e) {
+      e.preventDefault();
+
+      setSubmitted(true);
+      if (username && password) {
+        console.log('logging in');
+        const data = { username, password, token: 'mock-jwt' };
+        setTimeout(() => {
+          localStorage.setItem('user', JSON.stringify(data));
+          dispatch(userActions.login(data));
+        }, 2000);
+      }
+  }
+
+
   return (
     <div className="login">
       <h2>Login</h2>
+      <form name="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username</label>
+          <input type="text" name="username" value={username} onChange={handleChange} />
+          {submitted && !username &&
+            <div className="invalid-feedback">Username is required</div>
+          }
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" name="password" value={password} onChange={handleChange} />
+          {submitted && !password &&
+            <div className="invalid-feedback">Password is required</div>
+          }
+        </div>
+        <div>
+          <button>
+              {<span className="spinner-border spinner-border-sm mr-1"></span>}
+              Login
+          </button>
+          <Link to="/register" className="btn btn-link">Register</Link>
+        </div>
+      </form>
     </div>
   );
 }
