b2eeb67fd8eb8ca6ba72ece3caee4b8943751c78
aashnanaushad <36255953+aashnanaushad@users.noreply.github.com>
2020-04-14 14:52:33 +0530

    Added login and register pages

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import loginImg from "../../assets/login.svg";
+import './Login.css';
+//import axios from 'axios';
+
+
+function Login() {
+    const [email, setUserEmail] = useState('');
+    const [password, setUserPassword] = useState('');
+    
+    return (
+        <div className="base-container" >
+            <div className="header">Login</div>
+            <div className="content">
+                <div className="image">
+                    <img src={loginImg} alt="login image"/>
+                </div>
+                <div className="form">
+                    <div className="form-group">
+                        <label >Email</label>
+                        <input type="text" name="email" placeholder="email" value={email} onChange={e => setUserEmail(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" placeholder="password" value={password} onChange={e => setUserPassword(e.target.value)} /> 
+                     </div>
+                </div>
+            </div>
+            <div className="footer">
+                <button type="button"  className="btn">
+                    Login
+                </button>
+            </div>
+       </div>
+    );
+}
+ 
+export default Login;
