ec1675b43609a4a8921645f4de9905c2a00a2429
psjishnu <psjishnu13@gmail.com>
2020-04-14 16:44:15 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import './App.css';
 import './Index.css';
-
 import Navbar from './components/Navbar/Navbar';
 import SingleRoom from "./pages/Rooms/SingleRoom";
+
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
@@ -15,8 +14,6 @@ import Facilitator from './pages/Facilitator/Facilitator';
 import Rooms from './pages/Rooms/Rooms';
 import NotFound from './pages/NotFound/NotFound';
 import Book from "./pages/Book/Book";
-
-
 
 function App() {
   return (
@@ -29,11 +26,11 @@ function App() {
               <Route path="/about" exact component={About} />
               <Route path="/login" exact component={Login} />
               <Route path="/register" exact component={Register} />
-              <Route  path="/rooms/:slug" component={SingleRoom} />
               <Route path="/user" exact component={User} />
               <Route path="/facilitator" exact component={Facilitator} />
               <Route path="/rooms" exact component={Rooms} />
-              <Route path="/:slug/book" component={Book} />    
+              <Route path="/:slug/book" component={Book} /> 
+              <Route  path="/rooms/:slug" component={SingleRoom} /> 
               <Route component={NotFound} />
             </Switch>
           </main>
