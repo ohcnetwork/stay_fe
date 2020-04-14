838fe7717a78a75e6320c35e5a516eeb8defa3a3
aashnanaushad <36255953+aashnanaushad@users.noreply.github.com>
2020-04-14 15:41:48 +0530

    Add files via upload

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import loginImg from "../../assets/login.svg";
+import './Register.css';
 
 function Register() {
-  return (
-    <div className="register">
-      <h2>Register</h2>
-    </div>
-  );
-}
-
+    const [name, setUsername] = useState('');
+    const [email, setUserEmail] = useState('');
+    const [password, setUserpassword] = useState('');
+    const [cpassword, setUsercpassword] = useState('');
+ 
+    return (
+        <div className="base-container" >
+            <div className="header">Register</div>
+            <div className="content">
+                <div className="image">
+                   <img src={loginImg} alt="login image"/>
+                </div>
+                <form name="form">
+                   <div className="form-group">
+                        <label >Name</label>
+                        <input type="text" name="username" placeholder="username"  value={name} onChange={e => setUsername(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label >Email</label>
+                        <input type="text" name="email" placeholder="email" value={email} onChange={e => setUserEmail(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label >Password</label>
+                        <input type="text" name="password" placeholder="password" value={password} onChange={e => setUserpassword(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label >Confirm Password</label>
+                        <input type="text" name="cpassword" placeholder="confirm password" value={cpassword} onChange={e => setUsercpassword(e.target.value)}/>
+                    </div>
+                </form>
+            </div>
+            <div className="footer">
+               <button type="button" className="btn">
+                    Register
+                </button>
+            </div>
+        </div>
+    );
+  }
 export default Register;
