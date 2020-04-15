01b581833bed9f54c56a338c88644d13372675d2
psjishnu <psjishnu13@gmail.com>
2020-04-15 13:46:41 +0530

    test2

---
+++ src/Components/Context/Room_data.js
@@ -0,0 +1,721 @@
+import room1 from "../../Common/images/details-1.jpeg";
+import room2 from "../../Common/images/details-2.jpeg";
+import room3 from "../../Common/images/details-3.jpeg";
+import room4 from "../../Common/images/details-4.jpeg";
+import img1 from "../../Common/images/room-1.jpeg";
+import img2 from "../../Common/images/room-2.jpeg";
+import img3 from "../../Common/images/room-3.jpeg";
+import img4 from "../../Common/images/room-4.jpeg";
+import img5 from "../../Common/images/room-5.jpeg";
+import img6 from "../../Common/images/room-6.jpeg";
+import img7 from "../../Common/images/room-7.jpeg";
+import img8 from "../../Common/images/room-8.jpeg";
+import img9 from "../../Common/images/room-9.jpeg";
+import img10 from "../../Common/images/room-10.jpeg";
+import img11 from "../../Common/images/room-11.jpeg";
+import img12 from "../../Common/images/room-12.jpeg";
+
+export default [
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      hid:"h1" , capacity: 1,
+      pets: false,
+      location:"kochi",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "2",
+    },
+    fields: {
+      name: "single basic",
+      slug: "single-basic",
+      type: "single",
+      price: 150,
+      size: 250,
+      hid:"h3" , capacity: 1,
+      pets: false,
+      location:"kollam",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "3"
+    },
+    fields: {
+      name: "single standard",
+      slug: "single-standard",
+      type: "single",
+      price: 250,
+      size: 300,
+      hid:"h1" , capacity: 1,
+      pets: true,
+      location:"tvm",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "4"
+    },
+    fields: {
+      name: "single deluxe",
+      slug: "single-deluxe",
+      type: "single",
+      price: 300,
+      size: 400,
+      hid:"h3" , capacity: 1,
+      pets: true,
+      location:"kasargod",breakfast: true,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img4
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "5"
+    },
+    fields: {
+      name: "double economy",
+      slug: "double-economy",
+      type: "double",
+      price: 200,
+      size: 300,
+      hid:"h1" , capacity: 2,
+      pets: false,
+      location:"kochi",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img5
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "6"
+    },
+    fields: {
+      name: "double basic",
+      slug: "double-basic",
+      type: "double",
+      price: 250,
+      size: 350,
+      hid:"h2" , capacity: 2,
+      pets: false,
+      location:"kollam",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img6
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "7"
+    },
+    fields: {
+      name: "double standard",
+      slug: "double-standard",
+      type: "double",
+      price: 300,
+      size: 400,
+      hid:"h1" , capacity: 2,
+      pets: true,
+      location:"kollam",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img7
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "8"
+    },
+    fields: {
+      name: "double deluxe",
+      slug: "double-deluxe",
+      type: "double",
+      price: 400,
+      size: 500,
+      hid:"h1" , capacity: 2,
+      pets: true,
+      location:"kochi",breakfast: true,
+      featured: true,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img8
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "9"
+    },
+    fields: {
+      name: "family economy",
+      slug: "family-economy",
+      type: "family",
+      price: 300,
+      size: 500,
+      hid:"h1" , capacity: 3,
+      pets: false,
+      location:"tvm",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img9
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "10"
+    },
+    fields: {
+      name: "family basic",
+      slug: "family-basic",
+      type: "family",
+      price: 350,
+      size: 550,
+      hid:"h1" , capacity: 4,
+      pets: false,
+      location:"kasargod",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img10
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "11"
+    },
+    fields: {
+      name: "family standard",
+      slug: "family-standard",
+      type: "family",
+      price: 400,
+      size: 600,
+      hid:"h1" , capacity: 5,
+      pets: true,
+      location:"calicut",breakfast: false,
+      featured: false,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img11
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "12"
+    },
+    fields: {
+      name: "family deluxe",
+      slug: "family-deluxe",
+      type: "family",
+      price: 500,
+      size: 700,
+      hid:"h1" , capacity: 6,
+      pets: true,
+      location:"calicut",breakfast: true,
+      featured: true,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img12
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "13"
+    },
+    fields: {
+      name: "presidential",
+      slug: "presidential-room",
+      type: "presidential",
+      price: 600,
+      size: 1000,
+      hid:"h1" , capacity: 10,
+      pets: true,
+      location:"kochi",breakfast: true,
+      featured: true,
+      description:
+      "Desciption",
+      extras: [
+      "Feature1",
+      "Feature2",
+      "Feature3",
+      "Feature4",
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  }
+];
