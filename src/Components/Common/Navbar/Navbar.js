db4e7381d9c9a99e79c831222867f728082b4c71
Bodhisha Thomas <bodhisha@gmail.com>
2020-04-15 13:54:12 +0530

    Revert "Test2"

--- src/Components/Common/Navbar/Navbar.js
+++ src/Components/Common/Navbar/Navbar.js
@@ -1,57 +1,80 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import { userActions } from '../../../Redux/actions';
+import { userActions } from "../../../redux/actions";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
-  const user = useSelector(state => state.user);
+  const user = useSelector((state) => state.user);
 
   const [menuActive, setMenuActive] = useState(false);
 
   const dispatch = useDispatch();
 
   function menuToggle() {
-    (menuActive)? menuDisable(): setMenuActive(true);
+    menuActive ? menuDisable() : setMenuActive(true);
   }
-  
+
   function menuDisable() {
     setMenuActive(false);
   }
 
   function logoutUser() {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
     dispatch(userActions.logout());
   }
-
 
   return (
     <header className="navbar">
       <h1>Stay</h1>
-      <div role="button" className={`menu-btn ${menuActive? "is-active": ""}`} onClick={menuToggle}>
+      <div
+        role="button"
+        className={`menu-btn ${menuActive ? "is-active" : ""}`}
+        onClick={menuToggle}
+      >
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
-      <nav role="navigation" className={`${menuActive? "is-active": ""}`} onClick={menuDisable}>
-        { 
-          (user) 
-        ?
+      <nav
+        role="navigation"
+        className={`${menuActive ? "is-active" : ""}`}
+        onClick={menuDisable}
+      >
+        {user ? (
           <>
-            <Link to="/user" className="nav-link">Dashboard</Link>
-            <Link to="/rooms" className="nav-link">Rooms</Link>
-            <div role="button" onClick={logoutUser} className="nav-link nav-btn nav-btn-theme">Logout</div>
+            <Link to="/user" className="nav-link">
+              Dashboard
+            </Link>
+            <Link to="/rooms" className="nav-link">
+              Rooms
+            </Link>
+            <div
+              role="button"
+              onClick={logoutUser}
+              className="nav-link nav-btn nav-btn-theme"
+            >
+              Logout
+            </div>
           </>
-        :
+        ) : (
           <>
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/about" className="nav-link">About</Link>
-            <Link to="/login" className="nav-link nav-btn">Login</Link>
-            <Link to="/register" className="nav-link nav-btn nav-btn-theme">Register</Link>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+            <Link to="/login" className="nav-link nav-btn">
+              Login
+            </Link>
+            <Link to="/register" className="nav-link nav-btn nav-btn-theme">
+              Register
+            </Link>
           </>
-        }
+        )}
       </nav>
     </header>
   );
