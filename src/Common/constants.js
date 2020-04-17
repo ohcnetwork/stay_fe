5eeeb854c3a5413d5502b50f64515e0bea42164b
Gigin George <mail.gigin@gmail.com>
2020-04-17 13:47:36 +0530

    Remove old SRC

--- src/Common/constants.js
+++
@@ -1,6 +0,0 @@
-export const USER_TYPES = [
-  { type: "customer", string: "Customer" },
-  { type: "facilityowner", string: "Facility Owner" }
-];
-
-export const BASE_URL = "http://localhost:4009";
