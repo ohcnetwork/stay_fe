5aee43ad63621cac267f66985165a9ad5010df69
aashnanaushad <aashnananu@gmail.com>
2020-04-18 22:45:42 +0530

    room listing and room view ui and integration done

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,40 +1,55 @@
-import { fireRequest, actions } from './fireRequest';
+import { fireRequest, actions } from "./fireRequest";
 
 export const postRegister = (form) => {
-    return fireRequest('register', [], form);
+  return fireRequest("register", [], form);
 };
 export const postLogin = (form) => {
-    return fireRequest('login', [], form);
+  return fireRequest("login", [], form);
 };
 export const changePassword = (form) => {
-
-    return fireRequest('changepassword', [], form);
+  return fireRequest("changepassword", [], form);
 };
 
 export const postAddHotel = (form) => {
-    return fireRequest('addFacility',[],form);
+  return fireRequest("addFacility", [], form);
 };
 export const getBookingHistory = (form) => {
-    return fireRequest('bookingHistory',[form.id]);
+  return fireRequest("bookingHistory", [form.id]);
 };
 export const getCurrentUser = () => {
-    return fireRequest('currentUser');
+  return fireRequest("currentUser");
 };
 export const getUserHotelList = (id) => {
-    return fireRequest('userHotelList', [id]);
+  return fireRequest("userHotelList", [id]);
 };
 export const getHotelRoomList = (id) => {
-    return fireRequest('hotelRoomList', [id]);
+  return fireRequest("hotelRoomList", [id]);
 };
 export const getHotelBookingList = (id) => {
-    return fireRequest('hotelBookingList', [id]);
+  return fireRequest("hotelBookingList", [id]);
 };
 export const getHotelList = (params) => {
-    return fireRequest('getHotelDetails', [], params);
+  return fireRequest("getHotelDetails", [], params);
 };
 export const getOptionlist = () => {
-    return fireRequest('getOptionlist');
+  return fireRequest("getOptionlist");
 };
 export const getDistricts = () => {
-    return fireRequest('getDistricts');
+  return fireRequest("getDistricts");
+};
+
+export const getRoomByHotelid = (id) => {
+  return fireRequest("roomByHotelId", [id]);
+};
+export const getRoomByRoomid = (id) => {
+  return fireRequest("roomByRoomId", [id]);
+};
+export const getHotelByRoomid = (id) => {
+  return fireRequest("hotelByRoomId", [id]);
+};
+export const dopostBook = (userId, roomId, hotelId, body) => {
+  return fireRequest("postBook", [userId, roomId, hotelId], body);
+};
+export const changeRoomStatus = (roomid, status) => {
+  return fireRequest("changeRoomStatus", [roomid], status);
 };
--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,61 +1,90 @@
 export default {
-
   register: {
-    path: '/api/v1/auth/register',
-    method: 'POST',
-    noAuth: true
+    path: "/api/v1/auth/register",
+    method: "POST",
+    noAuth: true,
   },
   changepassword: {
-    path: '/api/v1/auth/change-password',
-    method: 'PUT',
-    noAuth: false
-},
-  addFacility:{
-    path:"/api/v1/facility/add-facility",
-    method:"POST",
-    noAuth: false
+    path: "/api/v1/auth/change-password",
+    method: "PUT",
+    noAuth: false,
+  },
+  addFacility: {
+    path: "/api/v1/facility/add-facility",
+    method: "POST",
+    noAuth: false,
   },
   login: {
-    path: '/api/v1/auth/login',
-    method: 'POST',
-    noAuth: true
+    path: "/api/v1/auth/login",
+    method: "POST",
+    noAuth: true,
   },
 
   currentUser: {
-    path: '/api/v1/auth/user',
-    noAuth: false
+    path: "/api/v1/auth/user",
+    noAuth: false,
   },
   bookingHistory: {
-    path: '/api/v1/booking/UserDetails',
-    method:'get',
-    noAuth: false
+    path: "/api/v1/booking/UserDetails",
+    method: "get",
+    noAuth: false,
   },
-  
+
   userHotelList: {
-    path: '/api/v1/facility',
-    noAuth: true
+    path: "/api/v1/facility",
+    noAuth: true,
   },
 
   hotelRoomList: {
-    path: '/api/v1/rooms/hotel',
-    noAuth: true
+    path: "/api/v1/rooms/hotel",
+    noAuth: true,
   },
 
   hotelBookingList: {
-    path: '/api/v1/booking/HotelDetails',
-    noAuth: true
+    path: "/api/v1/booking/HotelDetails",
+    noAuth: true,
   },
   getHotelDetails: {
-    path: '/api/v1/rooms',
-    method: 'GET',
-    noAuth: true
+    path: "/api/v1/rooms",
+    method: "GET",
+    noAuth: true,
   },
   getOptionlist: {
-    path: 'api/v1/rooms/get/details',
-    noAuth: true
+    path: "api/v1/rooms/get/details",
+    noAuth: true,
   },
   getDistricts: {
-    path: '/api/v1/facility/get/districts',
-    noAuth: true
-  }
-}
+    path: "/api/v1/facility/get/districts",
+    noAuth: true,
+  },
+
+  roomByHotelId: {
+    path: "/api/v1/rooms/hotel",
+    method: "GET",
+    noAuth: true,
+  },
+
+  roomByRoomId: {
+    path: "/api/v1/rooms",
+    method: "GET",
+    noAuth: true,
+  },
+
+  hotelByRoomId: {
+    path: "/api/v1/rooms/hotelid",
+    method: "GET",
+    noAuth: true,
+  },
+
+  postBook: {
+    path: "/api/v1/booking",
+    method: "POST",
+    noAuth: true,
+  },
+
+  changeRoomStatus: {
+    path: "/api/v1/rooms/status",
+    method: "PATCH",
+    noAuth: false,
+  },
+};
