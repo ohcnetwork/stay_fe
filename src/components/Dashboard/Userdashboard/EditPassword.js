c10bc91b3bbde9beafeeaf7a458f81693af4122d
psjishnu <psjishnu13@gmail.com>
2020-04-19 10:58:57 +0530

    updated routing

---
+++ src/components/Dashboard/Userdashboard/EditPassword.js
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import { useDispatch , useSelector} from "react-redux";
+import { changePassword  } from "../../../Redux/actions"
+import * as Notficiation from "../../../util/Notifications";
+
+export default function EditPassword(){
+    const dispatch = useDispatch();
+    const state = useSelector(state => state);
+    const { currentUser } = state;
+    const initForm = {
+        id:currentUser.data.data.id,
+        currentPassword:"",
+        password: "",
+        confirm: ""
+    };
+    const initError = {
+        id:currentUser.data.data.id,
+        currentPassword:"",
+        password: "",
+        confirm: "",
+    };
+    const [formLoading, setFormLoading] = useState(false);
+    const [form, setForm] = useState(initForm);
+    const [error, setError] = useState(initError);
+    const [formError, setFormError] = useState(false);
+
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        const fieldValue = { ...form }
+
+        // error handling needed
+
+        fieldValue[name] = name === "email" ?
+            value.toLowerCase() :
+            value
+
+        setForm(fieldValue)
+    };
+    function validInputs() {
+        let formValid = true;
+        let err = Object.assign({}, initError);
+        const { currentPassword,password, confirm } = form;
+
+        Object.keys(form).forEach(key => {
+            if (form[key] === "") {
+                formValid = false;
+                err[key] = "This field is required";
+            }
+        });
+        if (password !== confirm) {
+            err["confirm"] = "Passwords do not match";
+            formValid = false;
+        }
+        else if(password === currentPassword){
+            err["password"] = "Cannot be old password";
+            formValid = false;
+        }
+
+        setError(err);
+        return formValid;
+    }
+
+        const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (validInputs() && !formLoading) {
+
+            setFormLoading(true);
+            dispatch(changePassword(form)).then(resp => {
+                const { status: statusCode } = resp;
+                const { data: res } = resp;
+                // set captha logic needed
+                if (res && statusCode === 200 && res.success === true) {
+                    Notficiation.Success({
+                        msg: "Password Changed"
+                    });
+                    setForm(initForm);
+                }
+
+                let formErr = "Some problem occurred";
+                // error exists show error 
+                if (res && res.success === false && res.data) {
+                    formErr = Object.values(res.data)[0];
+
+                }
+              
+                const errorMessages = resp.response ? resp.response.data ? resp.response.data.message : null : null;
+                if (errorMessages) {
+                    let err = initError;
+                    errorMessages.forEach(msgObj => {
+                        err[msgObj.property] = Object.values(msgObj.constraints).map((val, i) => <p key={i.toString()}>{val}</p>);
+                    });
+                    setError(err);
+                }
+
+                        //setFormError(formErr);
+                setFormLoading(false);
+            });
+        }
+    }
+
+    return(
+
+        
+
+        <div className="max-w-md lg:ml-3 w-full">
+        <div>
+            <h2 className="mt-3 m-3 text-center text-3xl leading-9 font-bold text-gray-800 uppercase">
+                Change Password
+            </h2>
+        </div>
+        <form onSubmit={handleSubmit} className="bg-white mt-4 shadow-lg rounded px-8 pt-6 pb-8 my-20 bg-gray-200">
+        <div className="mb-4 md:mr-2 md:mb-0">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        aria-label="Password"
+                        name="currentPassword"
+                        type="password"
+                        value={form.currentPassword}
+                        onChange={handleChange}
+                        className={`shadow appearance-none border ${error.password ? "border-red-500" : ""} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                        placeholder="******************" />
+                    <div className="text-xs italic text-red-500">{error.password}</div>
+                </div>
+
+
+
+            <div className="mb-4 md:flex md:justify-between">
+                <div className="mb-4 md:mr-2 md:mb-0">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        aria-label="Password"
+                        name="password"
+                        type="password"
+                        value={form.password}
+                        onChange={handleChange}
+                        className={`shadow appearance-none border ${error.password ? "border-red-500" : ""} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                        placeholder="******************" />
+                    <div className="text-xs italic text-red-500">{error.password}</div>
+                </div>
+                <div className="md:ml-2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm">
+                        Confirm Password
+                    </label>
+                    <input
+                        aria-label="Confirm Password"
+                        name="confirm"
+                        type="password"
+                        value={form.confirm}
+                        onChange={handleChange}
+                        className={`shadow appearance-none border ${error.confirm ? "border-red-500" : ""} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                        placeholder="******************" />
+                    <div className="text-xs italic text-red-500">{error.confirm}</div>
+                </div>
+            </div>
+            <div className="h-10">
+                <p className="text-red-500 text-xs italic bold text-center mt-2">{formError}</p>
+            </div>
+            <div className="flex items-center justify-between sm:flex-row">
+                <button type="submit" className={`flex items-center ${formLoading ? "bg-gray-600" : "bg-indigo-600 hover:bg-indigo-800"} text-white font-bold py-2 px-4 sm:px-3 rounded focus:outline-none focus:shadow-outline`}>
+                    <svg className={`h-5 w-5 ${formLoading ? "text-gray-400" : "text-indigo-500"} transition ease-in-out duration-150 mr-1`} fill="currentColor" viewBox="0 0 20 20">
+                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                    </svg>
+                    Change Password
+                 </button>
+            </div>
+        </form>
+    </div>
+    )
+}
--- src/components/Dashboard/Userdashboard/UserDashboard.js
+++ src/components/Dashboard/Userdashboard/UserDashboard.js
@@ -1,55 +1,51 @@
 import React from "react";
 import { A } from "hookrouter";
-import { useSelector } from "react-redux";
+import {  useSelector } from 'react-redux';
+
+
 
 function Home() {
-  const defimage =
-    "https://blogtimenow.com/wp-content/uploads/2014/06/hide-facebook-profile-picture-notification.jpg";
 
-  //user=user[0];
-  const state = useSelector((state) => state);
-  const { currentUser } = state;
-  const user = currentUser.data.data;
-  //console.log(user);
-  console.log(user.image);
-  if (user.image == null) {
-    user.image = defimage;
-  }
+    const defimage="https://blogtimenow.com/wp-content/uploads/2014/06/hide-facebook-profile-picture-notification.jpg"
+     
+     //user=user[0];
+    const state = useSelector(state => state);
+    const { currentUser } = state;
+    const user=currentUser.data.data;
+    //console.log(user);
+    console.log(user.image);
+    if(user.image==null){
+        user.image=defimage;
+    }
   return (
-    <div className="mt-10 ">
-      <div class="bg-gray-200 py-3 lg:w-1/2 w-3/4 m-0 m-auto sm:max-w-full max-w-md rounded overflow-hidden shadow-lg">
-        <A
-          href="/edit"
-          class="bg-blue-500  hover:bg-blue-700 px-5 py-2 text-white font-bold h-2 w-full rounded-full"
-        >
-          Edit data
-        </A>
+
+
+
+<div className="mt-10 " >
+  <div class="bg-gray-200 py-3 lg:w-1/2 w-3/4 m-0 m-auto sm:max-w-full max-w-md rounded overflow-hidden shadow-lg"> 
+  <A href="/edit" class="bg-blue-500  hover:bg-blue-700 px-5 py-2 text-white font-bold h-2 w-full rounded-full">
+  Edit data
+</A>
         <div class="text-center p-6  border-b">
-          <img
-            class="h-40 w-40 rounded-full mx-auto"
-            src={user.image}
-            alt="Randy Robertson"
-          />
-          <p class="pt-2 text-xl font-semibold">{user.name}</p>
-          <p class="text-lg text-gray-800">{user.email}</p>
-          <p class="text-lg text-gray-800">{user.mobno}</p>
-          <div class="mt-5">
-            <A
-              href="/history"
-              class="border rounded-full py-2 bg-white px-4 text-md font-semibold text-black-700"
-            >
-              Booking History
-            </A>
-          </div>
-          <A
-            className="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8"
-            href="/browse"
-          >
-            BROWSE
-          </A>
-        </div>
+      <img class="h-40 w-40 rounded-full mx-auto" src={user.image} alt="Randy Robertson" />
+      <p class="pt-2 text-xl font-semibold">
+        {user.name}
+      </p>
+      <p class="text-lg text-gray-800">
+      {user.email}
+      </p>
+      <p class="text-lg text-gray-800">
+      {user.mobno}
+      </p>
+      <div class="mt-5">
+        <A href="/history" class="bg-white hover:bg-blue-500 text-blue-700 font-semibold mt-6 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        Booking history 
+        </A>
       </div>
     </div>
+  </div>
+</div>
+
   );
 }
 
