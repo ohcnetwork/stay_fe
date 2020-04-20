df562301ac8fb6f2625ec9c02f73d880b7fbf53e
Gigin George <mail.gigin@gmail.com>
2020-04-20 14:22:19 +0530

    Add Production API

--- src/Redux/fireRequest.js
+++ src/Redux/fireRequest.js
@@ -88,7 +88,7 @@ export const APIRequest = (key, path = [], params = {}, urlParam) => {
 
   // set authorization header in the request header
   const config = {
-    baseURL: "http://localhost:4009/",
+    baseURL: "http://api.stay.coronsafe.in/",
     headers: {},
   };
   if (!request.noAuth && localStorage.getItem("stay_access_token")) {
