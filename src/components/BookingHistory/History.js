7afd7291adeb16ae3c646527e6e979b2e63bdc9e
psjishnu <psjishnu13@gmail.com>
2020-04-21 15:41:48 +0530

    enhanced user historypage

--- src/components/BookingHistory/History.js
+++ src/components/BookingHistory/History.js
@@ -17,6 +17,7 @@ const dispatch = useDispatch();
 
 const [form, setForm] = useState({});
 const [form2, setForm2] = useState({});
+const [form3, setForm3] = useState("ALL");
 
 var i=0;
 
@@ -39,6 +40,7 @@ const Cancel = (e) =>{
 } 
 
 useEffect(() => {
+  console.log("here");
   dispatch(getBookingHistory()).then(resp => 
   { 
     const { data: res } = resp;
@@ -74,9 +76,21 @@ item=item.concat(form[count-1-i]);
           <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
             Booking History
           </h2>
+          <div className="flex mt-3  lg:text-md text-sm w-5/6 flex-row bg-gray-200 lg:w-1/2 m-0 m-auto ">
+            <div className="text-center w-1/3  px-3 py-2 m-1">
+              <button className="bg-white w-full hover:bg-blue-500 text-blue-700 font-semibold mt-1  hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded" onClick={() => setForm3("ALL")}>ALL</button>
+            </div>
+            <div className=" text-center w-1/3  px-3 py-2 m-1">
+              <button className="bg-white w-full hover:bg-blue-500 text-blue-700 font-semibold mt-1  hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded" onClick={() => setForm3("BOOKED")}>BOOKED</button>
+            </div>
+            <div className=" text-center w-1/3 px-3 py-2 m-1">
+              <button className="bg-white w-full hover:bg-blue-500 text-blue-700 font-semibold mt-1  hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded" onClick={() => setForm3("CANCELLED")}>CANCELLED</button>
+            </div>
+          </div>
         </div>
         <div className="relative  content-center  m-8 lg:mx-8 lg:my-4 lg:max-w-5xl">
             {item.map((value,index) =>  {
+              if(value.bookingStatus === form3 || form3 === "ALL")
                 return (
                     <div  className="sm:w-full lg:w-1/2 md:w-3/4 bg-gray-300 mx-auto my-8  rounded overflow-hidden shadow-lg">
                         <img className="w-full  h-30" src={DEFAULT_IMAGE.HOTEL} alt={value.name}/>
