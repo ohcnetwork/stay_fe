1987bb17511144aabc1f5f9050320e2ac4537d70
bodhisha <bodhisha@gmail.com>
2020-04-23 20:21:55 +0530

    update photoupload in addroom and edithotel pages

--- src/components/Facilitator/EditHotel.js
+++ src/components/Facilitator/EditHotel.js
@@ -409,27 +409,6 @@ export default function EditHotel({ id }) {
                         <div className="text-xs italic full-width text-red-500">{error.starCategory}</div>
                     </div>
 
-                    {/* File upload */}
-                    <div className="mt-2">
-                        <label className="block text-sm text-gray-600 " htmlFor="photos">
-                            Upload photos
-                        </label>
-
-                        <div className="flex w-full items-center px-5 bg-grey-lighter">
-                            <label className="w-20 flex flex-col items-center px-1 py-1 bg-white text-blue rounded-lg shadow-lg tracking-wide border border-blue cursor-pointer hover:bg-indigo-600 hover:text-white">
-                                <svg
-                                    className="w-5 h-5"
-                                    fill="currentColor"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                                </svg>
-                                <span className="mt-2 text-xs leading-normal">Select a file</span>
-                                <input type="file" className="hidden" />
-                            </label>
-                        </div>
-                    </div>
                     <div className="mt-2">
                         <label className="block text-sm text-gray-600" htmlFor="contact">
                             Contact Number
@@ -446,7 +425,6 @@ export default function EditHotel({ id }) {
                             aria-label="Name"
                         />
                         <div className="text-xs italic text-red-500">{error.contact}</div>
-
                     </div>
 
                     <div className="mt-2">
