ca164d7a8498b20c06c2a5388efdca4a1a45c573
psjishnu <psjishnu13@gmail.com>
2020-04-25 13:02:09 +0530

    updated register page

--- src/components/Account/UserRegister.js
+++ src/components/Account/UserRegister.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Register from "./Register";
 
 function UserRegister() {
   return (
-    <Register type={"user"} label={"REGISTER AS CUSTOMER"} othertype={"Hotel Owner"} otherlinkid={"1"}/>
+      <Register 
+        type={"user"}
+        label={"REGISTER AS CUSTOMER"}
+        othertype={"Hotel Owner"}
+        otherlinkid={"1"}
+      />
   )
 }
 
