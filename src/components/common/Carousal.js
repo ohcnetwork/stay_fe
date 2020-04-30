f8ae8775fcc9e4b5665c3165856793462400ce17
Nikhil Nath R <nikhilnath1000@gmail.com>
2020-04-30 16:18:44 +0530

    Fix error in Carousal
    - Previous image was shown if current image is not loaded
    - Added loader if it is not loaded
    - Hide carousal controls if images is 1

--- src/components/common/Carousal.js
+++ src/components/common/Carousal.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FullLoading } from "./Loader";
 
 export default function Carousal({ images, control, title = "" }) {
     const [shown, setShown] = control;
@@ -8,6 +9,7 @@ export default function Carousal({ images, control, title = "" }) {
 
     const [imageOpacity, setImageOpacity] = useState(1);
     const [animationRunning, setAnimationRunning] = useState(false);
+    const [loaded, setLoaded] = useState([images.map((el) => false)]);
 
     const initTransitionDuration = 300;
     const transitionDuration = 200;
@@ -36,7 +38,7 @@ export default function Carousal({ images, control, title = "" }) {
     }, [shown, images, timerInterval, transitionDuration]);
 
     useEffect(() => {
-        if (shown) {
+        if (shown && images.length > 1) {
             console.log("add listener");
             document.addEventListener("keydown", handleKeyPress);
             return () => {
@@ -93,6 +95,12 @@ export default function Carousal({ images, control, title = "" }) {
         }
     }
 
+    function loadHandler(num) {
+        const newLoaded = [...loaded];
+        newLoaded[num] = true;
+        setLoaded(newLoaded);
+    }
+
     if (!images || images.length < 1) {
         return null;
     }
@@ -105,51 +113,61 @@ export default function Carousal({ images, control, title = "" }) {
                 <div className="absolute top-0 text-shadow text-2xl tracking-wide md:text-4xl text-white pt-5 text-center">
                     {title}
                 </div>
+                {!loaded[currentImage] && (
+                    <div className="absolute">
+                        <FullLoading color="none" />
+                    </div>
+                )}
                 <img
+                    onLoad={() => loadHandler(currentImage)}
                     src={images[currentImage]}
                     alt={title}
-                    className={`max-h-full object-cover opacity-${imageOpacity} transition ease-in-out duration-${transitionDuration}`}
+                    className={`max-h-full object-cover opacity-${imageOpacity} transition ease-in-out duration-${transitionDuration} ${
+                        loaded[currentImage] ? "" : "opacity-0"
+                    }`}
                 />
-                <div className="absolute flex justify-between items-center w-full h-full">
-                    <div className="flex items-center justify-center h-full pl-2 sm:pl-5">
-                        <button
-                            onClick={previousImage}
-                            className="sm:p-5 p-2 flex items-center justify-center rounded-full text-white opacity-50 bg-black">
-                            <svg
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fas"
-                                data-icon="chevron-left"
-                                className="w-5 md:w-10 svg-inline--fa fa-chevron-left fa-w-10"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 320 512">
-                                <path
-                                    fill="currentColor"
-                                    d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path>
-                            </svg>
-                        </button>
+                {images.length > 1 && (
+                    <div className="absolute flex justify-between items-center w-full h-full">
+                        <div className="flex items-center justify-center h-full pl-2 sm:pl-5">
+                            <button
+                                onClick={previousImage}
+                                className="sm:p-5 p-2 flex items-center justify-center rounded-full text-white opacity-50 bg-black">
+                                <svg
+                                    aria-hidden="true"
+                                    focusable="false"
+                                    data-prefix="fas"
+                                    data-icon="chevron-left"
+                                    className="w-5 md:w-10 svg-inline--fa fa-chevron-left fa-w-10"
+                                    role="img"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 320 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex items-center justify-center h-full pr-2 sm:pr-5">
+                            <button
+                                onClick={nextImage}
+                                className="sm:p-5 p-2 flex items-center justify-center rounded-full text-white opacity-50 bg-black">
+                                <svg
+                                    aria-hidden="true"
+                                    focusable="false"
+                                    data-prefix="fas"
+                                    data-icon="chevron-right"
+                                    className="w-5 md:w-10 svg-inline--fa fa-chevron-right fa-w-10"
+                                    role="img"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 320 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
+                                </svg>
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex items-center justify-center h-full pr-2 sm:pr-5">
-                        <button
-                            onClick={nextImage}
-                            className="sm:p-5 p-2 flex items-center justify-center rounded-full text-white opacity-50 bg-black">
-                            <svg
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fas"
-                                data-icon="chevron-right"
-                                className="w-5 md:w-10 svg-inline--fa fa-chevron-right fa-w-10"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 320 512">
-                                <path
-                                    fill="currentColor"
-                                    d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
+                )}
                 <button
                     onClick={opacity === 1 ? () => setShown(false) : null}
                     className="absolute top-0 right-0 px-5 md:px-10 py-5 text-white">
