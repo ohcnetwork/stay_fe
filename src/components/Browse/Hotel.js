90e8bb5ef29971c306c44381f898f61e8f6d3f15
aashnanaushad <aashnananu@gmail.com>
2020-05-09 00:52:41 +0530

    fix: UI chnages in filters

--- src/components/Browse/Hotel.js
+++ src/components/Browse/Hotel.js
@@ -241,6 +241,14 @@ function Hotel() {
                                         </option>
                                     ))}
                                 </select>
+                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                    <svg
+                                        className="fill-current h-4 w-4"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
 
@@ -316,6 +324,7 @@ function Hotel() {
                             </label>
                             <div className="relative pt-2">
                                 <DatePicker
+                                    className="appearance-none  w-half bg-grey-lighter text-grey-darker  py-1 px-2"
                                     value={new Date(form.checkin)}
                                     onChange={(newdate) =>
                                         handleChange({
@@ -343,6 +352,7 @@ function Hotel() {
                             </label>
                             <div className="relative pt-2">
                                 <DatePicker
+                                    className="appearance-none  w-half bg-grey-lighter text-grey-darker  py-1 px-2"
                                     value={new Date(form.checkout)}
                                     onChange={(newdate) =>
                                         handleChange({
