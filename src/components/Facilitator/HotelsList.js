49bdcc201058c7f7b9594d4f8def6d3cc0f67291
Bodhisha Thomas <bodhisha@gmail.com>
2020-07-03 19:12:11 +0530

    Revert "Pagination"

--- src/components/Facilitator/HotelsList.js
+++ src/components/Facilitator/HotelsList.js
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Star from "../common/Star";
 import { HOTEL_STATUS } from "../../Common/constants";
 import { A } from "hookrouter";
-import Pagination from 'reactjs-hooks-pagination';
 
 export default function HotelsList({ hotelList = [], isAdmin }) {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const pageLimit = 4;
     function showHotels(data) {
         let hotels = Object.values(data);
-
         if (hotels.length > 0) {
-            setTotalRecords(hotels.length);
-
             return hotels.map((hotel) => (
-
                 <A
-
                     key={hotel.id.toString()}
                     href={
                         isAdmin
@@ -44,14 +35,12 @@ export default function HotelsList({ hotelList = [], isAdmin }) {
                             <div
                                 className={`text-sm py-1 px-2 bg-${
                                     HOTEL_STATUS[hotel.status].color
-                                    } text-white font-bold uppercase tracking-wide text-center`}>
+                                } text-white font-bold uppercase tracking-wide text-center`}>
                                 {HOTEL_STATUS[hotel.status].string}
                             </div>
                         </div>
                     </div>
                 </A>
-
-
             ));
         } else {
             return (
@@ -75,16 +64,6 @@ export default function HotelsList({ hotelList = [], isAdmin }) {
                 </div>
             </div>
             {hotelList && showHotels(hotelList)}
-            <div className="d-flex flex-row py-4 justify-content-end">
-                <Pagination
-                    totalRecords={totalRecords}
-                    pageLimit={pageLimit}
-                    pageRangeDisplayed={1}
-                    onChangePage={setCurrentPage}
-                />
-            </div>
         </div>
-
-
     );
 }
