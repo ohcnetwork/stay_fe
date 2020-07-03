cdce8483ea6b8fc6e54e15425640594396d10a81
aashnanaushad <aashnananu@gmail.com>
2020-07-04 00:41:16 +0530

    fixes in position of paginator

--- src/components/Browse/HotelList.js
+++ src/components/Browse/HotelList.js
@@ -62,67 +62,70 @@ const HotelList = ({ hotels, search, input }) => {
     }
     const pageNumbers = getPageNumbers();
     return (
-        <div className="hotelslist-center max-w-6xl mx-auto">
-            <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-4 md:grid-cols-3 md:max-w-none sm:mx-8">
-                {result.slice(offset, offset + maxLimit).map((item) => {
-                    return (
-                        <div key={item.id}>
-                            <A
-                                className="flex flex-col rounded-lg shadow-sm border overflow-hidden max-w-xs mx-auto hover:shadow-xl"
-                                href={`/roomlist/${item.id}`}>
-                                <div className="flex-shrink-0">
-                                    <img
-                                        className="h-48 w-full object-cover"
-                                        src={
-                                            (item.photos && item.photos[0]) ||
-                                            DEFAULT_IMAGE.HOTEL
-                                        }
-                                        alt={item.name}
-                                    />
-                                </div>
-                                <div className="block">
-                                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                                        <div className="flex-1">
-                                            <p className="text-sm leading-5 font-medium text-indigo-600 h-4">
-                                                <span className="flex">
-                                                    <Star
-                                                        num={item.starCategory}
-                                                        dim={4}
-                                                    />
-                                                </span>
-                                            </p>
-                                            <h3 className="mt-2 text-base truncate leading-7 font-semibold text-gray-900">
-                                                {item.name}
-                                            </h3>
-                                            {/* <p className="mt-3 text-xl leading-6 text-gray-500">
+        <div>
+            <div className="hotelslist-center max-w-6xl mx-auto">
+                <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-4 md:grid-cols-3 md:max-w-none sm:mx-8">
+                    {result.slice(offset, offset + maxLimit).map((item) => {
+                        return (
+                            <div key={item.id}>
+                                <A
+                                    className="flex flex-col rounded-lg shadow-sm border overflow-hidden max-w-xs mx-auto hover:shadow-xl"
+                                    href={`/roomlist/${item.id}`}>
+                                    <div className="flex-shrink-0">
+                                        <img
+                                            className="h-48 w-full object-cover"
+                                            src={
+                                                (item.photos && item.photos[0]) ||
+                                                DEFAULT_IMAGE.HOTEL
+                                            }
+                                            alt={item.name}
+                                        />
+                                    </div>
+                                    <div className="block">
+                                        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                                            <div className="flex-1">
+                                                <p className="text-sm leading-5 font-medium text-indigo-600 h-4">
+                                                    <span className="flex">
+                                                        <Star
+                                                            num={item.starCategory}
+                                                            dim={4}
+                                                        />
+                                                    </span>
+                                                </p>
+                                                <h3 className="mt-2 text-base truncate leading-7 font-semibold text-gray-900">
+                                                    {item.name}
+                                                </h3>
+                                                {/* <p className="mt-3 text-xl leading-6 text-gray-500">
                                             Rs. 18,000
                                        </p> */}
-                                        </div>
+                                            </div>
 
-                                        <div className="mt-6 flex items-center">
-                                            <div className="ml-3">
-                                                <p className="text-sm leading-5 font-medium text-gray-900">
-                                                    {item.category}
-                                                </p>
-                                                <div className="flex text-sm leading-5 text-gray-500 h-24">
-                                                    {/* <time dateTime="2020-03-16">
+                                            <div className="mt-6 flex items-center">
+                                                <div className="ml-3">
+                                                    <p className="text-sm leading-5 font-medium text-gray-900">
+                                                        {item.category}
+                                                    </p>
+                                                    <div className="flex text-sm leading-5 text-gray-500 h-24">
+                                                        {/* <time dateTime="2020-03-16">
                                                 Mar 16, 2020
                                             </time> */}
-                                                    {item.district}
-                                                    <br />
-                                                    {item.address}
-                                                    <span className="mx-1">
-                                                        &middot;
+                                                        {item.district}
+                                                        <br />
+                                                        {item.address}
+                                                        <span className="mx-1">
+                                                            &middot;
                                                     </span>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </A>
-                        </div>
-                    );
-                })}
+                                </A>
+                            </div>
+                        );
+                    })}
+                </div>
+
             </div>
             <div className="sm:flex-1 flex align-center justify-between">
                 <div>
